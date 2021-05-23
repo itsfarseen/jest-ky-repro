8cd5699d62a34167804cc93ce88eed75cddbc6b8
Farseen <zah4n5dz@anonaddy.me>
2021-05-24 02:53:12 +0530

    Removed even more files

--- src/backend/index.test.ts
+++ src/backend/index.test.ts
@@ -1,37 +1,8 @@
-import { z } from 'zod';
-import { zParser, dummyParser } from './validation';
-import { EndpointWithBody, fetchEndpoint } from './index';
+import { hello } from './index';
 import { describe, expect, test } from '@jest/globals';
 
-const endpoint: EndpointWithBody<{}, { token: string }, "LOGIN_FAILED"> = {
-  path: "",
-  method: "POST",
-  parseReq: dummyParser,
-  parseResp: zParser(z.object({
-    token: z.string()
-  })),
-  parseErr: zParser(z.literal("LOGIN_FAILED"))
-}
-
 describe("fetchEndpoint", () => {
-  test("Login Success", async () => {
-    let res = await fetchEndpoint({ ...endpoint, path: "http://localhost/auth/login_success" })
-    expect(res).toStrictEqual({ success: true, data: { token: "1234" } })
-  })
-
-  test("Login Success - validation error", async () => {
-    let res = await fetchEndpoint({ ...endpoint, path: "http://localhost/auth/login_success_invalid" })
-    expect(res).toStrictEqual({ success: true, data: { token: "1234" } })
-  })
-
-  test("Login Failed", async () => {
-    let res = await fetchEndpoint({ ...endpoint, path: "http://localhost/auth/login_failed" })
-    expect(res).toStrictEqual({ success: false, data: "LOGIN_FAILED" })
-  })
-
   test("Login Failed - validation error", async () => {
-    let res = await fetchEndpoint({ ...endpoint, path: "http://localhost/auth/login_failed_invalid" })
-    expect(res).toStrictEqual({ success: false, data: "LOGIN_FAILED" })
+      hello();
   })
-
 })
--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,41 +1,6 @@
 import ky from 'ky';
-import { Options as KyOptions } from 'ky/distribution/types/options'
-import { Parser } from './validation';
 
-export interface Endpoint<Resp, Err> {
-  path: string,
-  method: "GET" | "POST" | "PUT" | "DELETE" | "PATCH",
-  parseResp: Parser<Resp>,
-  parseErr: Parser<Err>,
-}
-
-export interface EndpointWithBody<Req, Resp, Err> extends Endpoint<Resp, Err> {
-  parseReq: Parser<Req>
-};
-
-export type Result<S, E> = {
-  status: "success",
-  data: S
-} | {
-  status: "error",
-  data: E
-};
-
-export async function fetchEndpoint<
-  Req,
-  RawReq,
-  Resp,
-  Err,
-  EP extends Endpoint<Resp, Err> | EndpointWithBody<Req, Resp, Err>
->(endpoint: EP, options?: { token?: string, body?: Req }): Promise<Result<Resp, Err>> {
-  let kyOptions: KyOptions = {
-    method: endpoint.method
-  };
-  if (options?.body != null) {
-    kyOptions.json = options.body;
-  }
-  if (options?.token != null) {
-    kyOptions.headers = { "Authorization": "Bearer " + options.token };
-  }
-  return ky(endpoint.path, kyOptions).json();
+export function hello() {
+    console.log(ky);
+    console.log("Hello World");
 }
